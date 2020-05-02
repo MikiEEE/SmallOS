2ea76cc2db23f8464b4e23834e12c1466126a2ee
MikiEEE <mikemp1997@gmail.com>
2020-05-01 21:34:46 -0500

    Finished basline commenting

--- shells.py
+++ shells.py
@@ -11,26 +11,26 @@ class baseShell():
 		if 'sw' in inpt_set:
 			# pdb.set_trace()
 			OS.toggleTerminal()
-			# OS.smShellPrint('CMD: ',end=' ')
-			# OS.smShellPrint('\n\n\n\n\n')
+			# OS.sPrint('CMD: ',end=' ')
+			# OS.sPrint('\n\n\n\n\n')
 		if 'stat' in inpt_set:
 			if len(inpt_set) == 1:
-				OS.smShellPrint(OS)
+				OS.sPrint(OS)
 			elif len(inpt_set) == 2:
 				task = OS.tasks.search(int(inpt.split()[-1]))
-				OS.smShellPrint(task.stat())
-			# OS.smShellPrint('CMD: ',end=' ')
+				OS.sPrint(task.stat())
+			# OS.sPrint('CMD: ',end=' ')
 		if 'count' in inpt_set:
 			# pdb.set_trace()
 			msg = ''.join([str(len(OS.tasks)),' process(es) running'])
-			OS.smShellPrint(msg,'\n')
-			# OS.smShellPrint('CMD: ',end=' ')
+			OS.sPrint(msg,'\n')
+			# OS.sPrint('CMD: ',end=' ')
 		if 'exit' in inpt_set:
 			sys.exit(0)
 		if 'kill' in inpt_set:
 			OS.tasks.delete(int(inpt.split()[-1]))
 		# if ''.join(inpt) != '':
-		OS.smShellPrint('CMD: ')
+		OS.sPrint('CMD: ')
 		# print('',end='')
 		return
 
